f6e0c5ddf0b39f0e7f7f8efb6bc726d4520570a7
PROLOVE <proLove0212@gmail.com>
2022-08-28 15:00:41 -0700

    Fixed shop loading screen

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState, Suspense, lazy } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Filters from "./Filters";
-
+import ProductCards from "./ProductCards";
 import ClipLoader from "react-spinners/ClipLoader";
 import server_url from "../../config/server";
-
-const ProductCards = lazy(() => import("./ProductCards"));
 
 const Shop = () => {
 	const [searchParams, setSearchParams] = useSearchParams({
@@ -15,12 +13,20 @@ const Shop = () => {
 	const [allProducts, setAllProducts] = useState();
 	const [products, setProducts] = useState([{ id: 1 }]);
 
+	const [loading, setLoading] = useState(true);
+
 	const category = searchParams.get("category");
 	const productName = searchParams.get("productName");
 
 	useEffect(() => {
 		const effect = async () => {
-			await fetchAllProducts(category, productName);
+			try {
+				await fetchAllProducts(category, productName);
+			} catch (error) {
+				return console.log(error.message);
+			} finally {
+				setLoading(false);
+			}
 		};
 		effect();
 		// eslint-disable-next-line
@@ -76,35 +82,39 @@ const Shop = () => {
 
 	return (
 		<>
-			<Filters categorizedProducts={categorizedProducts} />
-			{products.length ? (
+			{!loading ? (
 				<>
-					<div id="product-cards-container" className="flex justify-center">
-						<div
-							id="product-cards"
-							className="flex flex-col gap-8 justify-items-center grid-cols-2 my-8 xl:grid-cols-4 lg:grid-cols-3 md:grid"
-						>
-							<Suspense
-								fallback={
-									<div className="fixed flex w-full justify-center top-[38%] right-0">
-										<ClipLoader
-											color="#00A7DC"
-											size={150}
-											loading={true}
-											className="absolute"
-										/>
-									</div>
-								}
-							>
-								<ProductCards products={products} />
-							</Suspense>
-						</div>
-					</div>
+					<Filters categorizedProducts={categorizedProducts} />
+					{products.length ? (
+						<>
+							<div id="product-cards-container" className="flex justify-center">
+								<div
+									id="product-cards"
+									className="flex flex-col gap-8 justify-items-center grid-cols-2 my-8 xl:grid-cols-4 lg:grid-cols-3 md:grid"
+								>
+									<ProductCards products={products} />
+								</div>
+							</div>
+						</>
+					) : (
+						<h1 className="fixed text-center top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl">
+							{!loading && "No results"}
+						</h1>
+					)}
 				</>
 			) : (
-				<h1 className="fixed text-center top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl">
-					No results
-				</h1>
+				<>
+					{loading && (
+						<div className="fixed flex w-full justify-center top-[38%]">
+							<ClipLoader
+								color="#00A7DC"
+								size={150}
+								loading={true}
+								className="absolute"
+							/>
+						</div>
+					)}
+				</>
 			)}
 		</>
 	);
